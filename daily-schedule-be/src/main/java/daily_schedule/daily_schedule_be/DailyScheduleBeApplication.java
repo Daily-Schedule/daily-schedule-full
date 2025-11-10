a67d98e4e9e7f12c3c524b2d4df7ca2ad87cec2a
202110861 <dbsalsk2514@naver.com>
2025-11-10 21:35:14 +0900

    init: 초기 개발환경 세팅

---
+++ daily-schedule-be/src/main/java/daily_schedule/daily_schedule_be/DailyScheduleBeApplication.java
@@ -0,0 +1,13 @@
+package daily_schedule.daily_schedule_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyScheduleBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyScheduleBeApplication.class, args);
+	}
+
+}
