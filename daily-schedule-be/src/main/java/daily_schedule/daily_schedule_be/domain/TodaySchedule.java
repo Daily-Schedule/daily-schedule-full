cd5b5160cf8e64713a92d5bd55ccd3b0aa34df4f
OH WOONG KWON <oungtom1212@naver.com>
2025-11-13 21:29:32 +0900

    refactor: 부적절한 JPA 어노테이션 수정 및 DateTime 타입 변경
    - 각 필드에 컬럼 추가
    - start_time과 end_time LocalDateTime으로 타입 변경

--- daily-schedule-be/src/main/java/daily_schedule/daily_schedule_be/domain/TodaySchedule.java
+++ daily-schedule-be/src/main/java/daily_schedule/daily_schedule_be/domain/TodaySchedule.java
@@ -1,12 +1,10 @@
 package daily_schedule.daily_schedule_be.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalTime;
+import java.time.LocalDateTime;
 
 /**
  * schedule_detail 테이블의 Entity
@@ -18,15 +16,23 @@ import java.time.LocalTime;
 @Setter
 public class TodaySchedule {
     @Id
-    private String schedule_Id; // schedule_id (PK)
+    @Column(name = "schedule_id")
+    private String schedule_Id; // (PK)
 
-    private LocalTime startTime; // start_time
-    private LocalTime endTime; // end_time
-    private String content; // content
+    @Column(name = "start_time")
+    private LocalDateTime startTime;
+    @Column(name = "end_time")
+    private LocalDateTime endTime;
+    @Column
+    private String content;
 
-    private String scheduleResultDailyResultId; // schedule_result_daily_result_id
-    private String scheduleDateDateId; //schedule_date_date_Id
-    private String userId; // user_id
-    
+    // 다른 테이블과 연결되는 외래 키(FK)들
+    @Column(name = "schedule_result_daily_result_id")
+    private String scheduleResultDailyResultId;
+    @Column(name = "schedule_date_date_Id")
+    private String scheduleDateDateId;
+    @Column(name = "user_id")
+    private String userId;
+
     // @TODO 추후 생성자와 다른 메서드들 추가
 }
