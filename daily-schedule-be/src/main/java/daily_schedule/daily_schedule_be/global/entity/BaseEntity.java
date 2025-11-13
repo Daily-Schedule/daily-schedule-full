3483938e9915db225ff758ba13f22c64e90247e6
202110861 <dbsalsk2514@naver.com>
2025-11-13 16:17:40 +0900

    feat : Base entity 추가

---
+++ daily-schedule-be/src/main/java/daily_schedule/daily_schedule_be/global/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package daily_schedule.daily_schedule_be.global.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@SuperBuilder
+@MappedSuperclass
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
