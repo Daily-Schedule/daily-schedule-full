3d8fc4982e2f47b9064ace46ac2f365fd70441a9
hi2242 <doodukly1999@gmail.com>
2025-11-14 20:49:42 +0900

    fix(entity): ERD 수정에 따른 Entity 코드 수정

--- daily-schedule-be/src/main/java/daily_schedule/daily_schedule_be/domain/Schedule.java
+++ daily-schedule-be/src/main/java/daily_schedule/daily_schedule_be/domain/Schedule.java
@@ -15,36 +15,43 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 // AuditingEntityListener: 생성 시간을 자동 기록해주는 역할
 @EntityListeners(AuditingEntityListener.class)
-public class Todo {
+public class Schedule {
     // @Id: 해당 필드가 PK임을 알리는 어노테이션
     @Id
     // DB가 ID를 자동으로 증가하도록 생성
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;
 
     // DB 컬럼
     // NOT NULL -> nullable = false
     @Column(nullable = false)
-    private String author;
+    private String userId;
 
     @Column(nullable = false)
-    private LocalDateTime startAt;
+    private LocalDateTime startTime;
 
     @Column(nullable = false)
-    private LocalDateTime endAt;
+    private LocalDateTime endTime;
 
     @Column(nullable = false)
     private String content;
 
+    @Column(nullable = false)
+    private Long scheduleResultId;
+
     // 데이터 생성 시 자동 시간 기록
     @CreatedDate
     @Column(updatable = false)
-    private LocalDateTime createAt;
+    private LocalDateTime createdAt;
+
+    @CreateDate
+    @Column(updatable = true)
+    private LocalDateTime updatedAt;
 
     // DTO를 받아서 Entity를 생성하는 생성자
-    public Todo(LocalDateTime startAt, LocalDateTime endAt, String content) {
-        this.startAt = startAt;
-        this.endAt = endAt;
+    public Schedule(LocalDateTime startTime, LocalDateTime endTime, String content) {
+        this.startTime = startTime;
+        this.endTime = endTime;
         this.content = content;
     }
 }
