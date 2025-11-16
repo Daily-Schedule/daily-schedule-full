b53006a2be7fbe9fb41a6688d61cc0b5fa8f4be2
OH WOONG KWON <oungtom1212@naver.com>
2025-11-16 22:49:56 +0900

    comment: Entity 주석 변경 및 추가

--- daily-schedule-be/src/main/java/daily_schedule/daily_schedule_be/domain/today/entity/ScheduleResult.java
+++ daily-schedule-be/src/main/java/daily_schedule/daily_schedule_be/domain/today/entity/ScheduleResult.java
@@ -7,11 +7,12 @@ import lombok.Setter;
 import java.time.LocalDateTime;
 
 /**
- * schedule_result 테이블의 Entity
- * '일정 시작' API는 이 테이블의 `real_start_time`을 업데이트해야 함
+ * schedule_result 테이블과 1:1로 연결되는 엔티티
+ * <p>
+ * '일정 시작/종료' API는 이 엔티티의 정보를 수정(UPDATE)하는 것
  */
 @Entity
-@Table(name = "schedule_result")
+@Table(name = "schedule_result") // 연결된 테이블 이름
 @Getter
 @Setter
 public class ScheduleResult {
@@ -20,13 +21,17 @@ public class ScheduleResult {
     private Long id; // (PK)
 
     @Column(name = "real_start_time")
-    private LocalDateTime realStartTime;
+    private LocalDateTime realStartTime; // 실제 시작 시간
 
     @Column(name = "real_end_time")
-    private LocalDateTime realEndTime;
+    private LocalDateTime realEndTime; // 실제 종료 시간
 
+    /**
+     * '일정 종료' API가 호출되면, 이 필드가 'true'로 변환
+     * '어제' 탭에서 이 값으로 '완료 여부'를 판단
+     */
     @Column(name = "isFinished")
-    private boolean isFinished;
+    private boolean isFinished; // 완료 여부
 
     //
 }
--- daily-schedule-be/src/main/java/daily_schedule/daily_schedule_be/domain/today/entity/TodaySchedule.java
+++ daily-schedule-be/src/main/java/daily_schedule/daily_schedule_be/domain/today/entity/TodaySchedule.java
@@ -9,40 +9,45 @@ import lombok.Setter;
 import java.time.LocalDateTime;
 
 /**
- * schedule_detail 테이블의 Entity
- *
+ * schedule_detail 테이블과 1:1로 연결됨
+ * '내일 일정 API'가 이 엔티티를 생성
+ * '오늘 일정 API'는 이 엔티티를 '조회(GET)'하여 목록을 보여줌
  */
-@Entity
-@Table(name = "schedule_detail")
-@Getter
-@Setter
+@Entity // Spring에게 DB 테이블과 연결된다고 알려줌
+@Table(name = "schedule_detail") // 연결될 테이블의 이름은 'schedule_detail'
+@Getter // (Lombok) 이 엔티티의 정보(변수)를 꺼내는 Get 함수를 자동으로 생성
+@Setter // (Lombok) 이 엔티티의 정보를 수정하는 Set 함수를 자동으로 생성
 public class TodaySchedule extends BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 알아서 1씩 증가시킴
     private Long id; // (PK)
 
     @Column(name = "start_time", nullable = false)
-    private LocalDateTime startTime;
+    private LocalDateTime startTime; // 계획된 시작 시간
 
     @Column(name = "end_time", nullable = false)
-    private LocalDateTime endTime;
+    private LocalDateTime endTime;  // 계획된 종료 시간
 
     @Column(nullable = false)
-    private String content;
+    private String content; // 일정 내용
 
-    // 다른 테이블과 연결되는 외래 키(FK)들
-    @OneToOne(fetch = FetchType.LAZY)
+    /**
+     * '일정 결과' 엔티티와의 1:1 관계 매핑
+     * '일정 계획' 1개는 '일정 결과' 1개와 짝을 이룸
+     * <p>
+     * 이 '계획' 엔티티는 'schedule_result_id'라는 FK(외래 키) 컬럼을 통해
+     * 'ScheduleResult' 엔티티와 연결(Join)
+     */
+    @OneToOne(fetch = FetchType.LAZY) // 1:1 연결, 필요할 때만 불러옴
     @JoinColumn(name = "schedule_result_id")
-    private ScheduleResult scheduleResultId;
+    private ScheduleResult scheduleResultId; // 연결된 '일정 결과' 엔티티의 객체
 
-    // date 테이블 안쓰는걸로 임시 결정
-    // @ManyToOne(fetch = FetchType.LAZY)
-    // @JoinColumn(name = "schedule_date_id", nullable = false)
-    // private Schedule
-
+    /**
+     * 'user' (사용자) 엔티티와의 N:1 연결
+     * 여러 개(N)의 일정은 한 명(1)의 사용자에게 속한다.
+     * 이 일정은 '어떤 사용자'의 것인가?를 알려줌
+     */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
-
-    // @TODO 추후 생성자와 다른 메서드들 추가
 }
