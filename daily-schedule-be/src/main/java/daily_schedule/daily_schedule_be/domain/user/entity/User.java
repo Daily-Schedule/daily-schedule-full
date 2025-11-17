3f161830542c8da2a56fb3c46920dac6ecf242c1
202110861 <dbsalsk2514@naver.com>
2025-11-17 14:38:36 +0900

    refactor : ER 다이어그램 변화에 따른 User Entity 수정

--- daily-schedule-be/src/main/java/daily_schedule/daily_schedule_be/domain/user/entity/User.java
+++ daily-schedule-be/src/main/java/daily_schedule/daily_schedule_be/domain/user/entity/User.java
@@ -1,21 +1,24 @@
 package daily_schedule.daily_schedule_be.domain.user.entity;
 
+import daily_schedule.daily_schedule_be.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @Table(name = "user")
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-@Builder
+@SuperBuilder
 
-public class User{
+public class User extends BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true, length = 20)
     private String id;
 
-    @Column( length = 255)
+    @Column(nullable = false, length = 255)
     private String password;
 
     @Column(nullable = false, length = 100)
