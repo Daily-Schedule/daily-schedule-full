5f795bcd31f8b4b240e5dd96034ddb3b106c8cfc
202110861 <dbsalsk2514@naver.com>
2025-11-17 14:41:29 +0900

    feat : 회원 DTO 추가

---
+++ daily-schedule-be/src/main/java/daily_schedule/daily_schedule_be/domain/user/dto/request/UserRequestDTO.java
@@ -0,0 +1,29 @@
+package daily_schedule.daily_schedule_be.domain.user.dto.request;
+import jakarta.validation.constraints.NotBlank;
+
+public class UserRequestDTO {
+    public record Signup(
+            @NotBlank String id,
+            @NotBlank String password,
+            @NotBlank String nickname
+    ){
+        public String getId() {
+            return id;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+        public String getNickname() {
+            return nickname;
+        }
+    }
+    public record Login(
+            @NotBlank String id,
+            @NotBlank String password
+    ){
+        public CharSequence getPassword() {
+            return password;
+        }
+    }
+}
