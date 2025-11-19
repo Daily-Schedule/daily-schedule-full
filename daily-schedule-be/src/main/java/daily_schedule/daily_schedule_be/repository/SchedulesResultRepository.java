aea57346637dcc6bef3123ce1add4eb0c01a4712
윤종근 <137996885+hi2242@users.noreply.github.com>
2025-11-19 15:18:24 +0900

    Revert "Merge pull request #4 from Daily-Schedule/feature/BE/tomorrow-service"
    This reverts commit 58aa55b3badbbd42a30441e169787b759734c534, reversing
    changes made to 581869130b712b7afdd82b9460390ebef9f4f325.

--- daily-schedule-be/src/main/java/daily_schedule/daily_schedule_be/repository/SchedulesResultRepository.java
+++
@@ -1,7 +0,0 @@
-package daily_schedule.daily_schedule_be.repository;
-
-import daily_schedule.daily_schedule_be.domain.ScheduleResult;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SchedulesResultRepository extends JpaRepository<ScheduleResult, Long>{
-}
---
+++ daily-schedule-be/src/test/java/daily_schedule/daily_schedule_be/DailyScheduleBeApplicationTests.java
@@ -0,0 +1,13 @@
+package daily_schedule.daily_schedule_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyScheduleBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
