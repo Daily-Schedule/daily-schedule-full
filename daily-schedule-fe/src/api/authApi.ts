3a06c86da0eaaf89616662f92fcfdcb385cf874d
Mina-Yun <101165393+202110861@users.noreply.github.com>
2025-11-22 17:04:01 +0900

    feat : 회원가입, 로그인 API 코드 추가

--- daily-schedule-fe/src/api/authApi.ts
+++ daily-schedule-fe/src/api/authApi.ts
@@ -1,23 +1,24 @@
-import { api } from "@/lib/axios";
+import { axiosInstance } from "./common/axiosInstance";
 
-// 로그인 요청 시 보낼 데이터 타입
-export interface LoginRequest {
+export const PostRegister = async (data: {
   id: string;
-  password: string; // 백엔드 DTO는 password지만 form에서는 password로 씀
-}
+  password: string;
+  nickname: string;
+}) => {
+  console.log("data", data);
+  try {
+    const response = await axiosInstance.post("/api/user/register", data);
+    return response.data;
+  } catch (error) {
+    console.error("회원가입 실패", error);
+  }
+};
 
-// 로그인 성공 시 받을 데이터 타입 (UserResponseDTO 참고)
-export interface LoginResponse {
-  message: string;
-  token: string;
-  // 필요한 경우 tokenInfo, userInfo 등 추가
-}
-
-// 로그인 API 호출 함수
-export const loginUser = async (data: LoginRequest) => {
-  // 19번 줄 수정: 괄호 제거 및 순서 변경
-  // await가 api.post(...) 전체 결과를 기다리게 해야 합니다.
-  const response = await api.post<LoginResponse>("/user/login", data);
-
-  return response.data;
+export const PostLogin = async (data: { id: string; password: string }) => {
+  try {
+    const response = await axiosInstance.post("/api/user/login", data);
+    return response.data;
+  } catch (error) {
+    console.error("로그인 실패", error);
+  }
 };
