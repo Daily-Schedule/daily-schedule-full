a5bf472c8176fcd2afe3688504332ce89894c17b
Mina-Yun <101165393+202110861@users.noreply.github.com>
2025-11-23 13:44:21 +0900

    refactor : 불필요한 응답 데이터 삭제
    1. UserResponseDTO에서 UserTokenDTO tokenInfo 삭제

--- daily-schedule-be/src/main/java/daily_schedule/daily_schedule_be/domain/user/dto/response/UserResponseDTO.java
+++ daily-schedule-be/src/main/java/daily_schedule/daily_schedule_be/domain/user/dto/response/UserResponseDTO.java
@@ -3,7 +3,7 @@ package daily_schedule.daily_schedule_be.domain.user.dto.response;
 import lombok.Builder;
 
 @Builder
-public record UserResponseDTO(String message, String token, UserTokenDTO tokenInfo, UserInfo userInfo) {
+public record UserResponseDTO(String message, String token, UserInfo userInfo) {
 
 
     @Builder
